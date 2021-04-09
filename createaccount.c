187274e0652d66065ee4c0cbffb937fc83d863ff
Jihoon12 <81612552+Jihoon12@users.noreply.github.com>
2021-04-10 04:15:37 +0900

    Update createaccount.c

--- createaccount.c
+++ createaccount.c
@@ -1,11 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define MAX_NAME_LEN 21
 #define MAX_NUM_LEN 15
-#define MAX_PASS_LEN 5
-
+#define MAX_PASS_LEN 6 // 왜 6인지 찾지를 못했습니다..
+// 5를ㄹ 넣고 4자리 넣었는데 자꾸 자리수가 44가 나와서,..ㅠㅠㅠ...ㅠ흑
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <time.h>
 
 typedef struct Account
@@ -17,6 +18,7 @@ typedef struct Account
 }Account_t;
 
 void CreateAccount();
+
 Account_t accounts[100];
 
 int main()
@@ -33,7 +35,6 @@ void CreateAccount()
 
     int users_index = 0;
     accounts[users_index].money = 0;
-    strcpy(accounts[users_index].password, "0");
 
     accounts[users_index].num[0] = rand() % 8 + '1';
     for (int i = 1; i < 14; i++)
@@ -42,16 +43,40 @@ void CreateAccount()
     }
     accounts[users_index].num[14] = '\0';
 
+
+    char input_name[MAX_NAME_LEN];
+
     printf("계좌번호에 사용할 이름을 입력하세요.\n>>");
-    scanf("%s", accounts[users_index].name);
+    while (true)
+    {
+        fgets(input_name, MAX_NAME_LEN, stdin);
+        sscanf(input_name, "%s", accounts[users_index].name);
+        if (strlen(accounts[users_index].name) < MAX_NAME_LEN)
+        {
+            break;
+        }
+        else
+        {
+            printf("최대 이름이 초과하였습니다.\n 다시 입력해주세요.\n>>");
+        }
+    }
+
+
+    char input_password[MAX_PASS_LEN];
 
     printf("비밀번호 4자리를 입력하세요\n>>");
-    scanf("%s", accounts[users_index].password);
-
-    while (4 != strlen(accounts[users_index].password))
+    while (true)
     {
-        printf("비밀번호 4자리가 아닙니다.\n비밀번호 4자리를 다시 입력하세요.\n>>");
-        scanf("%s", accounts[users_index].password);
+        fgets(input_password, MAX_PASS_LEN, stdin);
+        sscanf(input_password, "%s", accounts[users_index].password);
+        if (strlen(accounts[users_index].password) == 4)
+        {
+            break;
+        }
+        else
+        {
+            printf("비밀번호 4자리가 아닙니다.\n 다시 입력해주세요.\n>>");
+        }
     }
 
     fprintf(fp, "이름:%s 계좌번호:%s 잔액:%d 비밀번호:%s\n", accounts[users_index].name, accounts[users_index].num, accounts[users_index].money, accounts[users_index].password);
