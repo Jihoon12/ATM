59fb98db456b767fa68f602222f1144d00100724
Jihoon12 <81612552+Jihoon12@users.noreply.github.com>
2021-04-11 17:39:21 +0900

    Update createaccount.c

--- createaccount.c
+++ createaccount.c
@@ -1,8 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define MAX_NAME_LEN 21
+#define MAX_NAME_LEN 22 // 이름 20 자리까지 \n \0
 #define MAX_NUM_LEN 15
-#define MAX_PASS_LEN 6 // 왜 6인지 찾지를 못했습니다..
-// 5를ㄹ 넣고 4자리 넣었는데 자꾸 자리수가 44가 나와서,..ㅠㅠㅠ...ㅠ흑
+#define MAX_PASS_LEN 6 // 비밀번호 4 자리 \n \0
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -36,10 +36,10 @@ void CreateAccount()
     int users_index = 0;
     accounts[users_index].money = 0;
 
-    accounts[users_index].num[0] = rand() % 8 + '1';
+    accounts[users_index].num[0] = rand() % 9 + '1';
     for (int i = 1; i < 14; i++)
     {
-        accounts[users_index].num[i] = rand() % 9 + '0';
+        accounts[users_index].num[i] = rand() % 10 + '0';
     }
     accounts[users_index].num[14] = '\0';
 
@@ -50,8 +50,9 @@ void CreateAccount()
     while (true)
     {
         fgets(input_name, MAX_NAME_LEN, stdin);
+        rewind(stdin);
         sscanf(input_name, "%s", accounts[users_index].name);
-        if (strlen(accounts[users_index].name) < MAX_NAME_LEN)
+        if (strlen(accounts[users_index].name) + 1 < MAX_NAME_LEN)
         {
             break;
         }
@@ -68,7 +69,10 @@ void CreateAccount()
     while (true)
     {
         fgets(input_password, MAX_PASS_LEN, stdin);
+        rewind(stdin);
         sscanf(input_password, "%s", accounts[users_index].password);
+
+        
         if (strlen(accounts[users_index].password) == 4)
         {
             break;
@@ -80,6 +84,5 @@ void CreateAccount()
     }
 
     fprintf(fp, "이름:%s 계좌번호:%s 잔액:%d 비밀번호:%s\n", accounts[users_index].name, accounts[users_index].num, accounts[users_index].money, accounts[users_index].password);
-
     fclose(fp);
 }
